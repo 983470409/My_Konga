afb0d3e4677eaabeb087b19e441cb3063759a12d
Zhangyuxiang <38565658+983470409@users.noreply.github.com>
2019-12-24 16:36:00 +0800

    导航窗格添加js代码

---
+++ assets/js/app/reqidcsetting/idcsettingnav.js
@@ -0,0 +1,18 @@
+addIdcSettingNav();
+
+
+
+
+
+
+function addIdcSettingNav() {
+    var idcHtmlDom = "<li data-ng-repeat=\"item in items\" data-ng-show=\"auth.authorize(item.access) &amp;&amp; item.show()\" data-ui-sref-active=\"active\" class=\"ng-scope\">\n" +
+        "\n" +
+        "                <!-- ngIf: item.state --><a data-ui-sref=\"services\" ng-if=\"item.state\" class=\"ng-scope\" href=\"#!/services\">\n" +
+        "                    <i class=\"mdi mdi-cloud-outline\"></i>\n" +
+        "                    <span class=\"text-small ng-binding\" ng-bind-html=\"item.title.toUpperCase()\">IDCSETTING</span>\n" +
+        "                </a><!-- end ngIf: item.state -->\n" +
+        "                <!-- ngIf: !item.state -->\n" +
+        "            </li>"
+    $(".side-nav__content").append(idcHtmlDom);
+}
